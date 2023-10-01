752254de51f4608990d4639d8108af0f2e5cbb79
Iamneo <techadmin@iamneo.ai>
2023-10-01 10:46:01 +0000

    Backup Commit

--- springapp/src/main/Student.java
+++ springapp/src/main/Student.java
@@ -1,29 +1,30 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-/*
-public class Student {
+
+public class Students {
     private int studentId;
     private String firstName;
     private String lastName;
 
     // Getters and setters
     // ...
-}*/
 
+
+}
 
 public class Student {
     public static void main(String[] args) {
         // Using Java-based configuration
         ApplicationContext javaContext = new AnnotationConfigApplicationContext(AppConfig.class);
-        Student javaStudent = javaContext.getBean("student", Student.class);
+        Students javaStudent = javaContext.getBean("students", Students.class);
         System.out.println("Java Config Student Details:");
         System.out.println("ID: " + javaStudent.getStudentId());
         System.out.println("First Name: " + javaStudent.getFirstName());
         System.out.println("Last Name: " + javaStudent.getLastName());
 
         // Using XML configuration
-        ApplicationContext xmlContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext xmlContext = new ClassPathXmlApplicationContext("./resources/applicationContext.xml");
         Student xmlStudent = xmlContext.getBean("student", Student.class);
         System.out.println("\nXML Config Student Details:");
         System.out.println("ID: " + xmlStudent.getStudentId());
