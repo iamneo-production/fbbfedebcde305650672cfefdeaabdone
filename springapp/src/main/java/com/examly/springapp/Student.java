67377aedc537d0c753d470b6876151d1a00fe957
Iamneo <techadmin@iamneo.ai>
2023-10-17 13:57:44 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/Student.java
+++ springapp/src/main/java/com/examly/springapp/Student.java
@@ -1,3 +1,5 @@
+package com.examly.springapp;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
